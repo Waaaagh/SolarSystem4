5afe9600622d38958346a4263625c3d55cc20693
Francis <sonsofix@yahoo.ca>
2015-02-15 01:56:44 -0800

    added new html CommentForm added new css for CommentForm added new js Forums new semi-functioning comment input etc

---
+++ solarSystem/js/Forums.js
@@ -0,0 +1,9 @@
+
+function showForm() {
+    document.getElementById("commentWindow").style.visibility = "visible";
+    }
+
+function hideForm() {
+    parent.window.close();
+    window.open("./Forums.html");
+    }
