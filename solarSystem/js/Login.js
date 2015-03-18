9ffbc1d4862629ceca0c0935bb62f8489ad20970
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2015-03-18 15:06:13 -0700

    .html -> Put proper capitalization on the ID's. .css -> Updated stuff with proper capitalization. .js -> Added a few examples to help us create new functions.

--- solarSystem/js/Login.js
+++ solarSystem/js/Login.js
@@ -1,2 +1,28 @@
 
+function $(id) {
+	var element = document.getElementById(id);
+	if (element == null)
+		alert("Programmer error:" + id + "does not exist.");
+	return element;
+}
+
+function testPassValid(id) {
+	if (radPassedWeb_0.checked == true){
+	return ($(id));
+	error = false;
+}
+}
+
+function testDateRangeValid(id) {
+	var day = txtStartDate_0;
+	var month = txtStartDate_1;
+	var year = txtStartDate_2;
+	if (day.value >= 1 && day.value <= 31 && month.value >= 1 && month.value <= 12 && year.value >= 2013 && year.value <= 2999){
+	return ($(id));
+	error = false;
+}
+}
+
+
+
 // Yeah, we slacked off on this part.
